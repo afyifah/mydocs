478400a689e0f271d7638e9fe3c5db0282ce0283
afyifah <mailwork.afifah@gmail.com>
2024-12-23 07:27:04 +0700

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,10 @@
 
 
 import { Button } from "@/components/ui/button"
-// import { buttonVariants } from "@/components/ui/button"
 
 
 import Image from "next/image";
-// import Link from "next/link";
+
 
 export default function Home() {
   return (
